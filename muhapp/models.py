1e28733cfd9f396e2df248d087adf465c6fa07ab
unknown <yusufzbayram@outlook.com>
2024-03-08 14:41:44 +0300

    onay sira numara 4 haneli yaildi

--- muhapp/models.py
+++ muhapp/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -13,7 +14,8 @@ class OnayRegisterModel(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     onay_no = models.CharField(max_length = 100)
     onay_aciklama = models.CharField(max_length = 100, default='my_default_value')
-    onay_tarih = models.CharField(max_length = 12)
+    #burda alani datefield sectik ve 'register_onay.html input olarak date yaptik. defter_onay_list.html icinde ise {{bilgi.onay_tarih|date:"d.m.Y"}} seklinde formatladik
+    onay_tarih = models.DateField(default=datetime.now) 
     onay_odemetutar = models.CharField(max_length = 20)
     onay_parabirimi = models.CharField(max_length = 20)
     onay_odemeyolu = models.CharField(max_length = 20)
