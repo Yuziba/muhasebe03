225eb9d7e13d2f1de9bab9490cff7d9206fe225b
unknown <yusufzbayram@outlook.com>
2024-03-10 19:54:01 +0300

    toplam tutar icin virgulle ayirma yapildi

--- amuhproje/settings.py
+++ amuhproje/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize', #ayilari 3basamakta nokta ile bolmek ici 
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,4 @@ LOGIN_REDIRECT_URL = "/"
 
 MEDIA_URL = '/media/'   
 MEDIA_ROOT = BASE_DIR / 'media'
+
